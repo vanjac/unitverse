d0b07eb6d54bc3a53a59355a7d66bd5ed76ae1f2
Jacob van't Hoog <vantjac@gmail.com>
2019-06-19 18:52:00 -0700

    Add FlipFlop unit

---
+++ Assets/Unitverse/FlipFlop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FlipFlop : MonoBehaviour
+{
+    public bool startOn, eventOnStart;
+    public UnityEvent turnOn, turnOff;
+    private bool _on;
+    public bool on
+    {
+        get
+        {
+            return _on;
+        }
+        set
+        {
+            if (_on != value)
+            {
+                _on = value;
+                if (_on)
+                    turnOn.Invoke();
+                else
+                    turnOff.Invoke();
+            }
+        }
+    }
+
+    void Awake()
+    {
+        _on = startOn;
+    }
+
+    void Start()
+    {
+        if (eventOnStart)
+        {
+            if (_on)
+                turnOn.Invoke();
+            else
+                turnOff.Invoke();
+        }
+    }
+
+    public void Toggle()
+    {
+        on = !on;
+    }
+}
