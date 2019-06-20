a8dc4ecfafcaf9e5ec6c1c4a5bd387a1413e20ea
Jacob van't Hoog <vantjac@gmail.com>
2019-06-19 19:04:35 -0700

    Add Counter unit

---
+++ Assets/Unitverse/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour
+{
+    public int count = 0, threshold = 1;
+    public bool eventOnStart;
+    public UnityEvent overThreshold, underThreshold;
+
+    void Start()
+    {
+        if (eventOnStart)
+        {
+            if (count >= threshold)
+                overThreshold.Invoke();
+            else
+                underThreshold.Invoke();
+        }
+    }
+
+    public void SetCount(int c)
+    {
+        int oldCount = count;
+        count = c;
+        if (oldCount < threshold && count >= threshold)
+            overThreshold.Invoke();
+        else if (oldCount >= threshold && count < threshold)
+            underThreshold.Invoke();
+    }
+
+    public void CountUp()
+    {
+        SetCount(count + 1);
+    }
+
+    public void CountDown()
+    {
+        SetCount(count - 1);
+    }
+}
