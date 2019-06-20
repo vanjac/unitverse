5fe1991e63c89c3a58651b5ea5886ed24897b9b5
Jacob van't Hoog <vantjac@gmail.com>
2019-06-19 18:36:14 -0700

    Add Relay unit

---
+++ Assets/Unitverse/Relay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Relay : MonoBehaviour
+{
+    public UnityEvent fire;
+
+    public void Fire()
+    {
+        fire.Invoke();
+    }
+}
