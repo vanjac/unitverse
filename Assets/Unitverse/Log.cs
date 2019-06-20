16e85ad7289a6293b738a56dbd91baec499ebcb1
Jacob van't Hoog <vantjac@gmail.com>
2019-06-19 18:18:21 -0700

    Add Log unit

---
+++ Assets/Unitverse/Log.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    public void LogMessage(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public void LogError(string message)
+    {
+        Debug.LogError(message);
+    }
+}
