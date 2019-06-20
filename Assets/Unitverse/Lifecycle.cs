2ba9bfa10d873e982f2fa8d085280be8a0d1c698
Jacob van't Hoog <vantjac@gmail.com>
2019-06-19 18:18:33 -0700

    Add lifecycle unit

---
+++ Assets/Unitverse/Lifecycle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Lifecycle : MonoBehaviour
+{
+    public UnityEvent awake, start, enable, disable, destroy;
+
+    void Awake()
+    {
+        awake.Invoke();
+    }
+
+    void Start()
+    {
+        start.Invoke();
+    }
+
+    void OnEnable()
+    {
+        enable.Invoke();
+    }
+
+    void OnDisable()
+    {
+        disable.Invoke();
+    }
+
+    void OnDestroy()
+    {
+        destroy.Invoke();
+    }
+}
