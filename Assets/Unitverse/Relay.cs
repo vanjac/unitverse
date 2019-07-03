010b5b94a35c86eab5d5e625b4fdf79f53b101f3
Jacob van't Hoog <vantjac@gmail.com>
2019-07-02 21:52:57 -0700

    Merge Counter, FlipFlop, Relay, and Switch into one unit

--- Assets/Unitverse/Relay.cs
+++ Assets/Unitverse/Relay.cs
@@ -5,10 +5,53 @@ using UnityEngine.Events;
 
 public class Relay : MonoBehaviour
 {
-    public UnityEvent fire;
+    public enum LimitAction
+    {
+        Continue, Stop, Wrap
+    }
+
+    public int branch;
+    public LimitAction limitAction;
+    public List<UnityEvent> events = new List<UnityEvent>() { new UnityEvent() };
 
     public void Fire()
     {
-        fire.Invoke();
+        if (branch >= 0 && branch < events.Count)
+            events[branch].Invoke();
+    }
+
+    public void SetBranch(int branch)
+    {
+        this.branch = branch;
+    }
+
+    public void CountUp()
+    {
+        branch += 1;
+        if (limitAction == LimitAction.Stop)
+        {
+            if (branch >= events.Count)
+                branch = events.Count - 1;
+        }
+        else if (limitAction == LimitAction.Wrap)
+        {
+            while (branch >= events.Count)
+                branch -= events.Count;
+        }
+    }
+
+    public void CountDown()
+    {
+        branch -= 1;
+        if (limitAction == LimitAction.Stop)
+        {
+            if (branch < 0)
+                branch = 0;
+        }
+        else if (limitAction == LimitAction.Wrap)
+        {
+            while (branch < 0)
+                branch += events.Count;
+        }
     }
 }
