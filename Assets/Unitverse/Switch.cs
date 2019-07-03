51f053c07aa49930b000894c8f2e0b2eaec1a2a7
Jacob van't Hoog <vantjac@gmail.com>
2019-07-02 20:45:14 -0700

    Add Switch unit

---
+++ Assets/Unitverse/Switch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour
+{
+    public int branch;
+    public List<UnityEvent> events;
+
+    public void SetBranch(int branch)
+    {
+        this.branch = branch;
+    }
+
+    public void Fire()
+    {
+        if (branch >= 0 && branch < events.Count)
+            events[branch].Invoke();
+    }
+}
