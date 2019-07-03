9c39e62cf140dd64e40b2dba7c8569f53d815f9d
Jacob van't Hoog <vantjac@gmail.com>
2019-07-02 20:50:53 -0700

    Add SendMessage unity

---
+++ Assets/Unitverse/SendMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessage : MonoBehaviour
+{
+    public string methodName = "Fire";
+
+    public void Broadcast(Component component)
+    {
+        component.BroadcastMessage(methodName, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void Send(Component component)
+    {
+        component.SendMessage(methodName, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void SendUpwards(Component component)
+    {
+        component.SendMessageUpwards(methodName, SendMessageOptions.DontRequireReceiver);
+    }
+}
