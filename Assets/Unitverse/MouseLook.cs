6425c3b45801d0842279fa475aaac45839e65491
Jacob van't Hoog <vantjac@gmail.com>
2019-07-18 06:09:11 -0700

    Add MouseLook unit

---
+++ Assets/Unitverse/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public string inputAxis = "Mouse X";
+    public float sensitivity = 5f;
+    public Vector3 rotateAxis = Vector3.up;
+    public bool clamp = false;
+    public float clampMin = -90f, clampMax = 90f;
+
+    void Update()
+    {
+        float deltaAngle = Input.GetAxis(inputAxis) * sensitivity;
+        var rotation = transform.localRotation * Quaternion.AngleAxis(deltaAngle, rotateAxis);
+        if (clamp)
+        {
+            rotation = rotation.normalized;
+            float angle = 2 * Mathf.Acos(rotation.w) * Mathf.Rad2Deg;
+            float dot = Vector3.Dot(rotateAxis, new Vector3(rotation.x, rotation.y, rotation.z));
+            if (dot < 0)
+                angle = -angle;
+            if (angle < clampMin)
+                rotation = Quaternion.AngleAxis(clampMin, rotateAxis);
+            else if (angle > clampMax)
+                rotation = Quaternion.AngleAxis(clampMax, rotateAxis);
+        }
+        transform.localRotation = rotation;
+    }
+}
