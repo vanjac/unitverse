42818d052e8b75bd00a58d8a240a6751048f6fb3
Jacob van't Hoog <vantjac@gmail.com>
2019-07-18 06:27:32 -0700

    Add InputButtonEvent unit

---
+++ Assets/Unitverse/InputButtonEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputButtonEvent : MonoBehaviour
+{
+    public string inputButton;
+    public UnityEvent down, up;
+
+    void Update()
+    {
+        if (Input.GetButtonDown(inputButton))
+            down.Invoke();
+        if (Input.GetButtonUp(inputButton))
+            up.Invoke();
+    }
+}
