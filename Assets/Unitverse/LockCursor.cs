3b917f393b2cb2c845d7b86974caeefef568087a
Jacob van't Hoog <vantjac@gmail.com>
2019-07-18 05:29:24 -0700

    Add LockCursor unit

---
+++ Assets/Unitverse/LockCursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCursor : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnDisable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
