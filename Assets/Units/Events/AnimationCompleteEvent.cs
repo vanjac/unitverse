f672b2fe0bf057bd883b87f2dffe1ed19cd121e3
j v <vantjac@gmail.com>
2021-03-31 14:11:44 -0700

    Add Animation Complete Event

---
+++ Assets/Units/Events/AnimationCompleteEvent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Units/Animation Complete Event")]
+public class AnimationCompleteEvent : MonoBehaviour
+{
+    [MyBox.AutoProperty]
+    public new Animation animation;
+    [SerializeField]
+    private string _animationName;
+    public string AnimationName
+    {
+        get { return _animationName; }
+        set { _animationName = value; }
+    }
+    public UltEvents.UltEvent complete;
+
+    private bool wasPlaying;
+
+    void OnEnable()
+    {
+        wasPlaying = false;
+    }
+
+    void Update()
+    {
+        bool isPlaying;
+        if (AnimationName == null || AnimationName == "")
+            isPlaying = animation.isPlaying;
+        else
+            isPlaying = animation.IsPlaying(AnimationName);
+        
+        if (!isPlaying && wasPlaying)
+            complete.Invoke();
+        wasPlaying = isPlaying;
+    }
+}
