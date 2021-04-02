a88283bb4c0c2669c5638541b0d6ded295897ed4
j v <vantjac@gmail.com>
2021-04-01 21:04:25 -0700

    add InputKeyEvent

---
+++ Assets/Units/Events/InputKeyEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Units/Events/Input Key Event")]
+public class InputKeyEvent : MonoBehaviour
+{
+    [SerializeField, MyBox.SearchableEnum]
+    private KeyCode _key;
+    public KeyCode Key
+    {
+        get { return _key; }
+        set { _key = value; }
+    }
+
+    public UltEvents.UltEvent down, up;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key) && down != null)
+            down.Invoke();
+        if (Input.GetKeyUp(Key) && up != null)
+            up.Invoke();
+    }
+}
