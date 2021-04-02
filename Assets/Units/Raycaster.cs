dacc8cb2e4de6f7e3b18572b49eb532c3d0c6df5
j v <vantjac@gmail.com>
2021-04-01 21:58:30 -0700

    Add raycaster unit

---
+++ Assets/Units/Raycaster.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[AddComponentMenu("Units/Raycaster")]
+public class Raycaster : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxDistance = Mathf.Infinity;
+    public float MaxDistance
+    {
+        get { return _maxDistance; }
+        set { _maxDistance = value; }
+    }
+
+    [SerializeField]
+    private LayerMask _mask = Physics.DefaultRaycastLayers;
+    public LayerMask Mask
+    {
+        get { return _mask; }
+        set { _mask = value; }
+    }
+
+    public QueryTriggerInteraction triggerInteraction
+        = QueryTriggerInteraction.UseGlobal;
+    
+    public UltEvents.UltEvent<Collider> hit;
+    public UltEvents.UltEvent miss;
+
+    public bool Raycast()
+    {
+        bool raycastHit = Physics.Raycast(transform.position, transform.forward,
+            out RaycastHit hitInfo, MaxDistance, Mask, triggerInteraction);
+        if (raycastHit && hit != null)
+            hit.Invoke(hitInfo.collider);
+        else if (!raycastHit && miss != null)
+            miss.Invoke();
+        return raycastHit;
+    }
+}
